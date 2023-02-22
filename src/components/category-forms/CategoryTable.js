50c793a0d09e822a237e995266a0b5f2b6a27032
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-02-22 23:33:21 +1100

    created axios helper for category

--- src/components/category-forms/CategoryTable.js
+++ src/components/category-forms/CategoryTable.js
@@ -1,1 +1,51 @@
-import 
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const CategoryTable = () => {
+  const dispatch = useDispatch();
+
+  const { categories } = useSelector((state) => state.category);
+
+  const [selectedCategory, setSelectedCategory] = useState({});
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  const handleOnDelete = (_id) => {
+    if (window.confirm("Are you sure you want to delete this category?")) {
+      dispatch(deleleCategory(_id));
+    }
+  };
+
+  const handleOnEdit = (item) => {
+    setSelectedCategory(item);
+    dispatch(setShowModal(true));
+  };
+
+  return (
+    <div className="mt-5">
+      <div>{categories.length} categories found!</div>
+
+      {/* have to add custom modal and edit categoryv form */}
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Status</th>
+            <th>Name</th>
+            <th>Slug</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* do table body section
+           */}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default CategoryTable;
--- src/helper/axios.js
+++ src/helper/axios.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 const rootUrl = "http://localhost:8000/api/v1";
 const adminApi = rootUrl + "/admin";
+const categoryApi = rootUrl + "/categories";
 
-export const postNewAdmin = async (data) => {
+const fetchProcesser = async ({ method, url, data }) => {
   try {
-    const res = await axios.post(adminApi + "/register", data);
+    const res = await axios({
+      method,
+      url,
+      data,
+    });
     return res.data;
   } catch (error) {
     return {
@@ -12,52 +17,95 @@ export const postNewAdmin = async (data) => {
       message: error.message,
     };
   }
+};
+
+export const postNewAdmin = async (data) => {
+  const url = adminApi + "/register";
+  const obj = {
+    method: "post",
+    url,
+    data,
+  };
+  return fetchProcesser(obj);
 };
 
 export const postEmailVerification = async (data) => {
-  try {
-    const res = await axios.post(adminApi + "/verify", data);
-    return res.data;
-  } catch (error) {
-    return {
-      status: "error",
-      message: error.message,
-    };
-  }
+  const url = adminApi + "/verify";
+  const obj = {
+    method: "post",
+    url,
+    data,
+  };
+  return fetchProcesser(obj);
 };
 
 export const loginAdmin = async (loginData) => {
-  try {
-    const { data } = await axios.post(adminApi + "/login", loginData);
-    return data;
-  } catch (error) {
-    return {
-      status: "error",
-      message: error.message,
-    };
-  }
+  const url = adminApi + "/login";
+  const obj = {
+    method: "post",
+    url,
+    data: loginData,
+  };
+  return fetchProcesser(obj);
 };
 
 export const fetchOtpRequest = async (formData) => {
-  try {
-    const { data } = await axios.post(adminApi + "/request-otp", formData);
-    return data;
-  } catch (error) {
-    return {
-      status: "error",
-      message: error.message,
-    };
-  }
+  const url = adminApi + "/request-otp";
+  const obj = {
+    method: "post",
+    url,
+    data: formData,
+  };
+  return fetchProcesser(obj);
 };
 
 export const resetPasswordRequest = async (formData) => {
-  try {
-    const { data } = await axios.post(adminApi + "/reset-password", formData);
-    return data;
-  } catch (error) {
-    return {
-      status: "error",
-      message: error.message,
-    };
-  }
+  const url = adminApi + "/reset-password";
+  const obj = {
+    method: "patch",
+    url,
+    data: formData,
+  };
+  return fetchProcesser(obj);
+};
+
+// category
+export const postCategory = async (data) => {
+  const url = categoryApi;
+  const obj = {
+    method: "post",
+    url,
+    data,
+  };
+  return fetchProcesser(obj);
+};
+
+export const fetchCategory = async () => {
+  const url = categoryApi;
+  const obj = {
+    method: "get",
+    url,
+  };
+  return fetchProcesser(obj);
+};
+
+//delete category
+export const deleleCategory = async (_id) => {
+  const url = categoryApi + "/" + _id;
+  const obj = {
+    method: "delete",
+    url,
+  };
+  return fetchProcesser(obj);
+};
+
+//update category
+export const updateCategory = async (data) => {
+  const url = categoryApi;
+  const obj = {
+    method: "put",
+    url,
+    data,
+  };
+  return fetchProcesser(obj);
 };
