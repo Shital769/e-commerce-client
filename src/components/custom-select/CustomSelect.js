49aa4c581313dd3abbcd05dcc74a6e17200d4605
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-04-01 15:30:02 +1100

    changes in custom-select file

--- src/components/custom-select/CustomSelect.js
+++ src/components/custom-select/CustomSelect.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Form } from "react-bootstrap";
 
-export const CustomSelect = ({name, args}) => {
+export const CustomSelect = ({ name, args, func, name, selectedCategory }) => {
+  useEffect(() => {}, [selectedCategory]);
+  console.log(selectedCategory);
   return (
     <Form.Group className="mb-3">
-        <label htmlFor=''>{label}</label>
-        <Form.Select name = {name} required >
-<option value="">Select category</option>
-{args.length}
-
-        </Form.Select>
+      <label htmlFor="">{label}</label>
+      <Form.Select
+        name={name}
+        required
+        onChange={func}
+        value={selectedCategory || ""}
+      >
+        <option value="">Select category</option>
+        {args.length > 0 &&
+          args.map(({ _id, name }) => (
+            <option key={_id} value={_id}>
+              {name}
+            </option>
+          ))}
+      </Form.Select>
     </Form.Group>
-  )
-}
+  );
+};
